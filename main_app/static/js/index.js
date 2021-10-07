fa556721c081181771540ebc22c666160a8ff075
kenne <kenneth.p.galvez@gmail.com>
2021-10-07 20:21:19 +0800

    Homepage and Index page full design

--- main_app/static/js/index.js
+++ main_app/static/js/index.js
@@ -4,3 +4,23 @@ function show_menu(){
 function close_menu(){
     $('.menu-list').hide();
 }
+function showModal(){
+    $('.item-modal').show();
+    $('.grey').show();
+    
+}
+
+if($('.item-modal:visible').length == 0)
+{
+    $('.grey').click(function (event) 
+    {
+       
+           if(!$(event.target).closest('.item-modal').length && !$(event.target).is('.item-modal')) {
+             $(".item-modal").hide();
+             $(".grey").hide();
+           }     
+    });
+}
+else{
+    
+}
