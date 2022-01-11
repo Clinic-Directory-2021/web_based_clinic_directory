18d32de173080a77cb132981a2fe09509f2af30f
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2022-01-11 12:06:17 +0800

    restrict appointment date to current date

--- main_app/static/js/base.js
+++ main_app/static/js/base.js
@@ -1,3 +1,17 @@
 function dashboard(){
     
 }
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+var yyyy = today.getFullYear();
+if(dd<10){
+  dd='0'+dd
+} 
+if(mm<10){
+  mm='0'+mm
+} 
+
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("datefield").setAttribute("min", today);
