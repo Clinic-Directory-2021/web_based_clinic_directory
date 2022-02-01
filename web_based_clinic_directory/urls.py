5bafb4ed290953ffeef97b2902719dda662bd125
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2022-02-02 03:43:19 +0800

    Update urls.py

--- web_based_clinic_directory/urls.py
+++ web_based_clinic_directory/urls.py
@@ -21,7 +21,7 @@ from django.views.static import serve
 # from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     # path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     # path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
